b8dbab3137f8710a912b2b448cb73e9639654347
= <joshyelc@gmail.com>
2020-06-16 14:26:42 -0600

    App get routes created

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,17 +1,14 @@
 <?php 
-    require_once('Router.php');
-    use Config\Routing\Router;
+    // require_once('Router.php');
+    require_once(SITE_ROOT . "/autoload.php");
+    use Config\Router;
 
-    Router::get('/pixiv', function(){
-        echo 'Pixiv';
-    });
+    Router::get('/','Controller#index');
+
+    Router::get('/pixiv', 'ControllerPixiv#index');
     
-    Router::get('/illustrator', function(){
-        echo 'Pixiv';
-    });
+    Router::get('/illustrator', 'ControllerIllustrator#index');
 
-    Router::get('/sauce', function(){
-        echo 'sauce';
-    });
+    Router::get('/sauce', 'ControllerSauce#index');
 
 ?>
