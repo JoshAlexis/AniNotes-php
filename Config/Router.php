2565bfe45a64cc2fb2bbc4f1e153effb22129594
= <joshyelc@gmail.com>
2020-06-16 14:25:55 -0600

    Router show views

--- Config/Router.php
+++ Config/Router.php
@@ -1,60 +1,63 @@
 <?php  
-    // include_once('../Controllers');
-    namespace Config\Routing;
-    use Controllers;
-
+    namespace Config;
+    require_once(SITE_ROOT . "/autoload.php");
+    require_once(SITE_ROOT . "/Controllers/ControllerPixiv.php");
+    require_once(SITE_ROOT . "/Controllers/ControllerIllustrator.php");
+    require_once(SITE_ROOT . "/Controllers/ControllerSauce.php");
+    use Controllers\ControllerPivix;
+    use Controllers\ControllersIllustrator;
+    
+    /**
+     * Provides methods to create routes and it's control
+     */
     class Router{
 
         private static $routesList = [];
+        private static $lastRouteUsed = [];
         
-        public static function add($route, $function){
-            array_push(self::$routesList,[
-                'uri' => $route,
-                'controller' => $function
-            ]);
-            
-        }
-
-        public static function redirect($route){
-            foreach(self::$routesList as $rou){
-                if($rou == $route){
-                    
-                }
-            }
-        }
-
         public static function run(){
             $path =  parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
             $method = $_SERVER['REQUEST_METHOD'];
-
+            
+            $matched_route = [];
             foreach(self::$routesList as $route){
                 if($route['uri'] == $path && $route['method'] == $method){
-                    $elements = explode('#',$route['controller']);
-                    $controller = $elements[0];
-                    $function = $elements[1];
-                    call_user_func([$controller, $function]);
+                    $matched_route = $route;
+                    self::$lastRouteUsed == $route;
                 }
             }
+            $elements = explode('#',$matched_route['controller']);
+            $controller = self::getControllerInstance($elements[0]);
+            $function = $elements[1];
+            call_user_func([$controller, $function]);
         }
 
         public static function get($route,$controller){
             array_push(self::$routesList, [
                 'uri' => $route,
-                'function' => $controller,
-                'controller' => 'GET'
+                'controller' => $controller,
+                'method' => 'GET'
             ]);
         }
 
         public static function post($route,$controller){
             array_push(self::$routesList, [
                 'uri' => $route,
-                'function' => $controller,
-                'controller' => 'POST'
+                'controller' => $controller,
+                'method' => 'POST'
             ]);
         }
 
         public static function allRoutes(){
             print_r(self::$routesList);
+        }
+
+        private static function getControllerInstance($controller){
+            if($controller == 'ControllerPixiv'){
+                return new ControllerPivix();
+            }else if($controller == 'ControllerIllustrator'){
+                return new ControllersIllustrator();
+            }
         }
     }
 ?>
