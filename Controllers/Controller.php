4f575e713b97f8e8d363eb5b2e77b71f28f1c79d
= <joshyelc@gmail.com>
2020-06-16 20:37:30 -0600

    Changed view to render

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -3,7 +3,7 @@ namespace Controllers;
 
 class Controller{
     public static function index(){
-        require_once(SITE_ROOT . "/Views/pixiv.php");
+        require_once(SITE_ROOT . "/Views/index.php");
     }
 }
 
