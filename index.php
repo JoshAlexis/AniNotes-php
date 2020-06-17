1a31b575cce00fe2a9213a38cf5406400785b94b
= <joshyelc@gmail.com>
2020-06-16 20:36:05 -0600

    Comments removed

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php 
-    /* require_once(SITE_ROOT . "/Config/Router.php");
-    require_once(SITE_ROOT . "/Config/Routes.php"); */
     require_once('./autoload.php');
     require_once(SITE_ROOT . "/Config/Routes.php");
     use Config\Router;
