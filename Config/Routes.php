8b1ec3087cbd1e95259e100b7bced5324a130564
= <joshyelc@gmail.com>
2020-06-16 20:36:55 -0600

    added more routes

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,14 +1,17 @@
 <?php 
-    // require_once('Router.php');
     require_once(SITE_ROOT . "/autoload.php");
     use Config\Router;
 
     Router::get('/','Controller#index');
 
     Router::get('/pixiv', 'ControllerPixiv#index');
-    
+    Router::get('/pixiv/edit/','ControllerPixiv#showEdit');
+
     Router::get('/illustrator', 'ControllerIllustrator#index');
+    Router::get('/illustrator/edit/', 'ControllerIllustrator#showEdit');
+    Router::post('/illustrators/edit/[0-9]', 'ControllerIllustrator#edit');
 
     Router::get('/sauce', 'ControllerSauce#index');
+    Router::get('/sauce/edit/','ControllerSauce#showEdit');
 
 ?>
