8dfee21d5e2bd5c2c95ef83cc4b90f389d415895
= <joshyelc@gmail.com>
2020-08-06 13:26:24 -0600

    Delete fetch method

--- models/Model.php
+++ models/Model.php
@@ -2,5 +2,4 @@
 abstract class Model{
     abstract protected function insert(Model $item, PDO $con): bool;
     abstract protected function update(Model $item, PDO $con): bool;
-    abstract protected function getAll(PDO $con): array;
 }
