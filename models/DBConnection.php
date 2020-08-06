d03e7e183c831655c78459d9e42429218e3d71e8
= <joshyelc@gmail.com>
2020-08-06 13:25:04 -0600

    Create class with connection method

--- models/DBConnection.php
+++ models/DBConnection.php
@@ -1,15 +1,19 @@
 <?php 
-
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'aninotes');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    $con = null;
-    try {
-        $dsn = "mysql:host=${DB_HOST};dbname=${DB_NAME}";
-        $con = new PDO($dsn, DB_USER, DB_PASS);
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+class DBConnection{
+    public static function connect(): PDO{
+        $DB_HOST = 'localhost';
+        $DB_NAME = 'aninotes';
+        $DB_USER = 'root';
+        $DB_PASS = '';
+        $con = null;
+        try {
+            $dsn = "mysql:host={$DB_HOST};dbname={$DB_NAME}";
+            $con = new PDO($dsn, $DB_USER, $DB_PASS);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+        return $con;
     }
+}
 
