ee3e82e0f7c1c07bce413a3a3906307f2bb044b9
= <joshyelc@gmail.com>
2020-08-07 20:16:34 -0600

    Insert data working and error message

--- addPixiv.php
+++ addPixiv.php
@@ -1,3 +1,29 @@
+<?php 
+require_once "./models/DBConnection.php";
+require_once "./models/Pixiv.php";
+
+$con = DBConnection::connect();
+if(isset($_POST['idPixiv'])){
+    $pixiv = new Pixiv();
+    $pixiv->idPixiv = strval($_POST['idPixiv']);
+    $pixiv->pixivName = $_POST['pixivName'];
+    $pixiv->content = $_POST['Content'];
+    $pixiv->quality = $_POST['Quality'];
+    $pixiv->favorite = $_POST['Favorite'];
+    $pixiv->link = $_POST['Link'];
+
+    if(($result = $pixiv->insert($con)) === true){
+        $con = null;
+        $_SESSION['message'] = "sucess";
+        $_SESSION['msg_info'] = "Insert Sucessful";
+        header("location: pixiv.php");
+    }else{
+        $_SESSION['message'] = "error";
+        $_SESSION['msg_info'] = $result;
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +45,18 @@
                 <a href="./pixiv.php">⬅</a>
             </div>
         </nav>
+        <?php if(isset($_SESSION['message'])){?>
+            <div class="row">
+                <div class="col-md-4 m-auto pt-4">
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <p><?= $result ?></p>
+                        <button type="button" class="close" data-diss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        <?php }?>
         <div class="row">
             <div class="col-md-12">
                 <h1 class="text-center text-primary">Add a Pixiv</h1>
@@ -27,11 +65,11 @@
         <!-- Form Input -->
         <section class="row">
             <div class="col-md-4 m-auto">
-                <form method="POST" action="./pixiv.php">
+                <form method="POST" action="./addPixiv.php">
                     <div class="form-group">
                         <label for="idPixiv">IdPixiv</label>
                         <input type="number" name="idPixiv" class="form-control"
-                        >
+                        required>
                     </div>
                     <div class="form-group">
                         <label for="pixivName">Pixiv Name</label>
@@ -39,7 +77,7 @@
                     </div>
                     <div class="form-group">
                         <label for="Content">Content</label>
-                        <textarea name="Content" class="form-control"></textarea>
+                        <textarea name="Content" class="form-control" required></textarea>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
@@ -63,7 +101,7 @@
                     </div>
                     <div class="form-group">
                         <label for="Link">Pixiv Link</label>
-                        <input type="url" name="Link" class="form-control">
+                        <input type="url" name="Link" class="form-control" required>
                     </div>
                     <div class="form-group text-center">
                         <input type="submit" value="addPixiv" class="btn btn-block btn-success"/>
@@ -72,8 +110,9 @@
             </div>
         </section>
     </div>
-    <script src="./js/jquery-3.5.1.min.js"></script>
+    <!-- <script src="./js/jquery-3.5.1.min.js"></script>
     <script src="./js/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script> 
+    <script src="./js/bootstrap.min.js"></script> --> 
+    <script src="./js/app.js"></script>
 </body>
 </html>
